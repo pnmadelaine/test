b5c888a9b3cac2e27b28f0fc824dc824c012cd0c
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-02-20 23:22:10 +0100

    update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    typhon.url = "github:typhon-ci/typhon/pnm/clearenv";
+    typhon.url = "github:typhon-ci/typhon/pnm/wip/clearenv";
     nixpkgs.follows = "typhon/nixpkgs";
   };
   outputs = {
@@ -15,25 +15,11 @@
       hello = pkgs.writeTextDir "index.html" "Hello world!";
       failure = pkgs.stdenv.mkDerivation {name = "failure";};
     };
-    #typhonProject = typhon.lib.github.mkProject {
-    #  owner = "pnmadelaine";
-    #  repo = "test";
-    #  secrets = pkgs.writeText "secrets" "";
-    #  typhonUrl = "https://example.com";
-    #};
-    typhonProject = typhon.lib.builders.mkProject {
-      actions.begin = typhon.lib.builders.mkActionScript {
-        mkPath = system: let
-          pkgs = import nixpkgs {inherit system;};
-        in [pkgs.nix pkgs.jq];
-        mkScript = system: ''
-          url=$(cat | jq -r '.input.url')
-          HOME=$(pwd)
-          export HOME
-          rev=$(nix --extra-experimental-features "nix-command flakes" eval --json --expr "builtins.parseFlakeRef \"$url\"" | jq -r '.rev')
-          echo "$rev" >&2
-        '';
-      };
+    typhonProject = typhon.lib.github.mkProject {
+      owner = "pnmadelaine";
+      repo = "test";
+      secrets = pkgs.writeText "secrets" "";
+      typhonUrl = "https://example.com";
     };
   };
 }
