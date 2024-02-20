b6ff31623cca41d7d2d2e8117488a9be2a78bf50
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-02-20 23:04:40 +0100

    debug

--- flake.nix
+++ flake.nix
@@ -15,11 +15,23 @@
       hello = pkgs.writeTextDir "index.html" "Hello world!";
       failure = pkgs.stdenv.mkDerivation {name = "failure";};
     };
-    typhonProject = typhon.lib.github.mkProject {
-      owner = "pnmadelaine";
-      repo = "test";
-      secrets = pkgs.writeText "secrets" "";
-      typhonUrl = "https://example.com";
+    #typhonProject = typhon.lib.github.mkProject {
+    #  owner = "pnmadelaine";
+    #  repo = "test";
+    #  secrets = pkgs.writeText "secrets" "";
+    #  typhonUrl = "https://example.com";
+    #};
+    typhonProject = typhon.lib.builders.mkProject {
+      actions.begin = typhon.lib.builders.mkActionScript {
+        mkPath = system: let
+          pkgs = import nixpkgs {inherit system;};
+        in [pkgs.nix pkgs.jq];
+        mkScript = system: ''
+          url=$(cat | jq '.input.url')
+          rev=$(nix --extra-experimental-feature nix-command eval --json --expr "builtins.parseFlakeRef \"$url\"" | jq -r '.rev')
+          echo "$rev" >&2
+        '';
+      };
     };
   };
 }
