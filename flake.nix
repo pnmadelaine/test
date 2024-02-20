14f4628dddbb3cc8da70c8d91d7838df6e34681e
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-02-20 22:11:32 +0100

    debug

--- flake.nix
+++ flake.nix
@@ -15,11 +15,21 @@
       hello = pkgs.hello;
       failure = pkgs.stdenv.mkDerivation {name = "failure";};
     };
-    typhonProject = typhon.lib.github.mkProject {
-      owner = "pnmadelaine";
-      repo = "test";
-      secrets = ./secrets.age;
-      typhonUrl = "https://example.com";
+    #typhonProject = typhon.lib.github.mkProject {
+    #  owner = "pnmadelaine";
+    #  repo = "test";
+    #  secrets = pkgs.writeTextFile "secrets" "";
+    #  typhonUrl = "https://example.com";
+    #};
+    typhonProject = typhon.lib.builders.mkProject {
+      actions.begin = typhon.lib.builders.mkActionScript {
+        mkPath = system: let
+          pkgs = import nixpkgs {inherit system;};
+        in [pkgs.coreutils pkgs.jq];
+        mkScript = _: ''
+          cat | jq '.input'
+        '';
+      };
     };
   };
 }
